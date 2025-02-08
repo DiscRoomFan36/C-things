7822e501c714824873f94fe3580a86995e9fc80f
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-02-08 16:42:29 +1100

    added get_all_dirs

--- src/file.h
+++ src/file.h
@@ -25,6 +25,14 @@ typedef struct String_Array {
     u64 capacity;
 } String_Array;
 
+#ifndef DT_REG
+#define DT_REG 8
+#endif // DT_REG
+
+#ifndef DT_DIR
+#define DT_DIR 4
+#endif // DT_DIR
+
 
 void get_all_files_in_directory(char *directory, String_Array *array) {
     DIR *dir = opendir(directory);
@@ -40,7 +48,7 @@ void get_all_files_in_directory(char *directory, String_Array *array) {
         if (!entry) break;
 
         // check its a file
-        if (entry->d_type != 8) continue;
+        if (entry->d_type != DT_REG) continue;
 
         // add the new string
         int name_len = strlen(entry->d_name);
@@ -48,7 +56,6 @@ void get_all_files_in_directory(char *directory, String_Array *array) {
         assert(new_string != NULL);
 
         strcpy(new_string, entry->d_name);
-        // new_string[name_len] = '\0';
 
         da_append(array, new_string);
     }
@@ -57,6 +64,38 @@ void get_all_files_in_directory(char *directory, String_Array *array) {
 
     if (closedir(dir) != 0) {} // an error occurred, we dont actually care...
 }
+
+void get_all_dirs_in_directory(char *directory, String_Array *array) {
+    DIR *dir = opendir(directory);
+
+    if (!dir) {
+        printf("ERROR when opening directory: %s\n", strerror(errno));
+        return;
+    }
+
+    while (True) {
+        errno = 0;
+        struct dirent *entry = readdir(dir);
+        if (!entry) break;
+
+        // check its a direct
+        if (entry->d_type != DT_DIR) continue;
+
+        // add the new string
+        int name_len = strlen(entry->d_name);
+        char *new_string = malloc((name_len+1) * sizeof(char));
+        assert(new_string != NULL);
+
+        strcpy(new_string, entry->d_name);
+
+        da_append(array, new_string);
+    }
+
+    if (errno) printf("ERROR when reading directory: %s\n", strerror(errno));
+
+    if (closedir(dir) != 0) {} // an error occurred, we dont actually care...
+}
+
 
 // TODO use String_View
 SV read_entire_file(char *filename) {
