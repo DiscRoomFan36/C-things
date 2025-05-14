76e16e1dfc9efc58675944239623701563358eec
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-05-14 12:08:43 +1000

    move String_View.h into own folder, i should do this for every project in mytb, turns out i dont actually want the full stb

---
+++ String_View/main.c
@@ -0,0 +1,27 @@
+//
+// Demo for String_View.h
+//
+// DiscRoomFan
+//
+// created - 14/05/2025
+//
+// build and run:
+// $ make && ./main
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define STRING_VIEW_IMPLEMENTATION
+#include "String_View.h"
+
+int main(int argc, char const **argv) {
+
+    SV my_string = SV_from_C_Str("Hello, World");
+    printf("my_string: "SV_Fmt"\n", SV_Arg(my_string));
+
+    return 0;
+}
+
