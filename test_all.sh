d47590976a5c6335931ea8d26e3522b0695306b2
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-06-30 08:12:26 +1000

    test everything, just to look for compiler warnings.

---
+++ test_all.sh
@@ -0,0 +1,56 @@
+
+set -xe
+
+cd ./Arena
+make && ./main >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+# echo "Testing Context"
+cd ../context
+make && ./main >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../JSON_Parser
+make main && ./main >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../misc
+make tests
+./build/file_test >> /dev/null
+./build/hashmap_test >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../Multi_Buddy
+make all
+./main >> /dev/null && ./main_unity >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../profiler
+make && ./main 10000000 >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../String_Builder
+make && ./main >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+cd ../String_View
+make && ./main >> /dev/null
+make clean >> /dev/null
+echo ""
+
+
+echo "All Tests Run."
