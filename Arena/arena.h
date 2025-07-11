42a43f48b00cfdfdc8f784eb72662ef09e390221
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-07-11 21:15:14 +1000

    Arena.h: its a single file header now.

--- Arena/arena.h
+++ Arena/arena.h
@@ -2,8 +2,8 @@
 // arena.h
 //
 // DiscRoomFan
-// created  - 03/03/2025
-// modified - 16/05/2025
+// Created  - 03/03/2025
+// Modified - 11/07/2025
 //
 // This is originally inspired by:
 // - https://github.com/tsoding/arena
@@ -15,8 +15,41 @@
 #define ARENA_H_
 
 
-// for number definitions, aka s64 and u64
-#include "ints.h"
+
+#ifndef INTS_DOT_H
+#define INTS_DOT_H
+
+    //
+    // ints.h - just some ints that are nice to have
+    //
+    // Author   - Fletcher M
+    //
+    // Created  - 21/03/2025
+    // Modified - 11/07/2025
+    //
+
+    #include <stdint.h>
+
+    typedef uint64_t   u64;
+    typedef uint32_t   u32;
+    typedef uint16_t   u16;
+    typedef uint8_t    u8;
+
+    typedef int64_t    s64;
+    typedef int32_t    s32;
+    typedef int16_t    s16;
+    typedef int8_t     s8;
+
+    typedef u32        bool32;
+
+    typedef float      f32;
+    typedef double     f64;
+
+    #define True       (0 == 0)
+    #define False      (0 != 0)
+
+#endif // INTS_DOT_H
+
 
 
 #ifndef ARENA_ASSERT
